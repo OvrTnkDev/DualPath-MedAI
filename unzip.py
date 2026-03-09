3e0ce4c57a260c0fe862a363414f17fcc5285670
Fabio D'Alessandro <137040853+OvrTnkDev@users.noreply.github.com>
2026-03-09 11:00:16 +0100

    chore: rimozione definitiva dataset dalla history e fix .gitignore

---
+++ unzip.py
@@ -0,0 +1,14 @@
+#unzippo i file che sono in data/datazip e li porto in data
+import zipfile
+import os
+
+def unzip_file(zip_file_path, extract_to_folder):
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to_folder)
+        
+if __name__ == "__main__":
+    zip_file_path = 'data/datazip/archive.zip'  # Percorso del file zip
+    extract_to_folder = 'data/clinical'  # Cartella di destinazione per i file estratti
+    
+    unzip_file(zip_file_path, extract_to_folder)
+    print(f"File estratti da {zip_file_path} a {extract_to_folder}")
