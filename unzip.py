a2e877dbf692e5876b0bcdeee0e754c7b2c3ad8c
Fabio D'Alessandro <137040853+OvrTnkDev@users.noreply.github.com>
2026-03-09 11:06:10 +0100

    cmt

--- unzip.py
+++ unzip.py
@@ -7,8 +7,18 @@ def unzip_file(zip_file_path, extract_to_folder):
         zip_ref.extractall(extract_to_folder)
         
 if __name__ == "__main__":
+    
+    #------------------zip1------------------
     zip_file_path = 'data/datazip/archive.zip'  # Percorso del file zip
     extract_to_folder = 'data/clinical'  # Cartella di destinazione per i file estratti
+    
+    unzip_file(zip_file_path, extract_to_folder)
+    print(f"File estratti da {zip_file_path} a {extract_to_folder}")
+    
+    #------------------zip2------------------
+    
+    zip_file_path = 'data/datazip/lish-moa.zip'
+    extract_to_folder = 'data/moa'
     
     unzip_file(zip_file_path, extract_to_folder)
     print(f"File estratti da {zip_file_path} a {extract_to_folder}")
